4b56fe5aa2ae65cea8e94c76072b7a1ffede7e9d
Chukuka <oochonogor@nibss-plc.com.ng>
2023-08-03 12:56:50 +0100

    UPDATED firewall commands with elevated privileges and removes IF conditional

--- proxy/nat-gw-firewall.sh
+++ proxy/nat-gw-firewall.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/bash
+#! /bin/bash
 
 # src:"https://blog.redbranch.net/2015/07/30/centos-7-as-nat-gateway-for-private-network/"
 
@@ -11,44 +12,41 @@
 # Implement change dynamically to avoid rebooting
 # sysctl -w net.ipv4.ip_forward=1
 
-# Check if Firewall is installed
+# Check if IPTables and Firewalld is installed
 systemctl status firewalld
-if [ $? -eq 0 ]
-then
-    sudo yum update
-    sudo yum install iptables-services
-    sudo sysctl -w net.ipv4.ip_forward=1
-    sudo /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-    sudo service iptables save
-    sudo yum install firewalld
-fi
+sudo yum update
+sudo yum install iptables-services
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo service iptables save
+sudo yum install firewalld
 
 # Requires at least 2 network interfaces
-firewall-cmd --zone=external --add-interface=eth0 --permanent
-firewall-cmd --zone=internal --add-interface=eth1 --permanent
+sudo firewall-cmd --zone=external --add-interface=eth0 --permanent
+sudo firewall-cmd --zone=internal --add-interface=eth1 --permanent
 
 # Ensure edits are applied into running configuration
-firewall-cmd --complete-reload
+sudo firewall-cmd --complete-reload
 
 # Check internal and external zones for edits
-firewall-cmd --list-all-zones
+sudo firewall-cmd --list-all-zones
 
 # Removes eth0 interface from internal zone
-firewall-cmd --zone=internal --remove-interface=eth0
+sudo firewall-cmd --zone=internal --remove-interface=eth0
 
-firewall-cmd --zone=external --add-masquerade --permanent
+sudo firewall-cmd --zone=external --add-masquerade --permanent
 
-# firewall-cmd --permanent --direct --passthrough ipv4 -t nat -I POSTROUTING -o eth0 -j MASQUERADE -s 10.89.0.0/16
+# sudo firewall-cmd --permanent --direct --passthrough ipv4 -t nat -I POSTROUTING -o eth0 -j MASQUERADE -s 10.89.0.0/16
 
-firewall-cmd --permanent --zone=internal --add-service=dhcp
-firewall-cmd --permanent --zone=internal --add-service=tftp
-firewall-cmd --permanent --zone=internal --add-service=dns
-firewall-cmd --permanent --zone=internal --add-service=http
-firewall-cmd --permanent --zone=internal --add-service=https
-firewall-cmd --permanent --zone=internal --add-service=nfs
-firewall-cmd --permanent --zone=internal --add-service=ssh
+sudo firewall-cmd --permanent --zone=internal --add-service=dhcp
+sudo firewall-cmd --permanent --zone=internal --add-service=dns
+sudo firewall-cmd --permanent --zone=internal --add-service=http
+sudo firewall-cmd --permanent --zone=internal --add-service=https
+# sudo firewall-cmd --permanent --zone=internal --add-service=tftp
+# sudo firewall-cmd --permanent --zone=internal --add-service=nfs
+# sudo firewall-cmd --permanent --zone=internal --add-service=ssh
 
 # Reload then show all allowed services and zones 
-firewall-cmd --complete-reload
-firewall-cmd --permanent --zone=internal --list-services
-firewall-cmd --list-all-zones
+sudo firewall-cmd --complete-reload
+sudo firewall-cmd --permanent --zone=internal --list-services
+sudo firewall-cmd --list-all-zones
